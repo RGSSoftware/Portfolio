83e60b6b672689147fd167720ec606c1f25b6c31
PC <MBA2@pcs-macbook-air.gateway.2wire.net>
2013-05-06 14:05:55 -0500

    created aboutMeController and renamed About.xib to aboutMeController.xib. Linked nib file to Controller

---
+++ Portfolio/aboutMeViewController.h
@@ -0,0 +1,13 @@
+//
+//  aboutMeViewController.h
+//  Portfolio
+//
+//  Created by PC on 5/6/13.
+//  Copyright (c) 2013 PC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aboutMeViewController : UIViewController
+
+@end
---
+++ Portfolio/aboutMeViewController.m
@@ -0,0 +1,38 @@
+//
+//  aboutMeViewController.m
+//  Portfolio
+//
+//  Created by PC on 5/6/13.
+//  Copyright (c) 2013 PC. All rights reserved.
+//
+
+#import "aboutMeViewController.h"
+
+@interface aboutMeViewController ()
+
+@end
+
+@implementation aboutMeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
